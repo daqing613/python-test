8739f949e6f202955abe4c7745d760d53069d64e
git <xoxo1987@live.cn>
2015-03-13 16:17:44 +0800

    Initial commit

---
+++ err.py
@@ -0,0 +1,31 @@
+##def foo(s):
+##    n = int(s)
+###    print '>>> n = %d' % n
+##    assert n != 0, 'n is zero!'
+##    return 10 / n
+##
+##def main():
+##    foo('0')
+##
+##main()
+
+##import logging
+##logging.basicConfig(level=logging.INFO)
+##
+##s = '0'
+##n = int(s)
+##logging.info('n = %d' % n)
+##print 10/n
+
+# pdb
+##s = '0'
+##n = int(s)
+##print 10 / n 
+
+
+import pdb
+
+s = '0'
+n = int(s)
+pdb.set_trace()
+print 10 / n 
